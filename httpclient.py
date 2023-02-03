43a0f2b7b0f18f842a211acb1d2e9e3a742dcfcc
Tommy-Sand <rosansandanasamy@gmail.com>
2023-02-02 23:50:13 -0700

    Passing 3 freetests.py tests

--- httpclient.py
+++ httpclient.py
@@ -31,23 +31,70 @@ class HTTPResponse(object):
     def __init__(self, code=200, body=""):
         self.code = code
         self.body = body
+    def __str__(self):
+        return "{}\n{}\n".format(self.code, self.body)
 
 class HTTPClient(object):
     #def get_host_port(self,url):
 
+    def parse_url_and_connect(self, url):
+        if not url.startswith("http://"):
+            url = "http://" + url
+
+        if not hasattr(self, 'parsed_url'):
+            self.parsed_url = urllib.parse.urlparse(url, scheme='http')
+
+        port = 80
+        if ( self.parsed_url.port != None ):
+            port = self.parsed_url.port
+        if not hasattr(self, 'socket'):
+            try:
+                self.connect(self.parsed_url.hostname, port)
+            except:
+                try:
+                    port = 8080
+                    self.connect(self.parsed_url.hostname, port)
+                except:
+                    return HTTPResponse(-1, "Cannot connect to {}".format(url))
+        return None
+
     def connect(self, host, port):
+        address = socket.gethostbyname(host)
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect((host, port))
+        self.socket.connect((address, port))
         return None
 
     def get_code(self, data):
-        return None
+        #get status code
+        line = data.split("\n")[0]
+        status_line = line.split(" ")
+        if(len(status_line) >= 3 and status_line[1].isnumeric()):
+            return int(status_line[1])
+        else:
+            return -1
 
     def get_headers(self,data):
-        return None
+        #get headers
+        headers = dict()
+        for line in data.split("\n")[1:]:
+            if(line == "\r"):
+                break
+            try:
+                header, value = line.split(":", 1)
+                headers[header] = value
+            except:
+                headers["CLIENT-ERROR-OCCURED"] = "True"
+                break
+        return headers
 
     def get_body(self, data):
-        return None
+        lines = data.split("\n")[1:]
+        body = ""
+        for i in range(len(lines)):
+            if(lines[i] == "\r"):
+                body = "\n".join(lines[i:]).strip()
+                break
+        return body
     
     def sendall(self, data):
         self.socket.sendall(data.encode('utf-8'))
@@ -61,23 +108,48 @@ class HTTPClient(object):
         done = False
         while not done:
             part = sock.recv(1024)
-            if (part):
-                buffer.extend(part)
-            else:
-                done = not part
+            if not part:
+                break
+            buffer.extend(part)
         return buffer.decode('utf-8')
 
     def GET(self, url, args=None):
-        code = 500
-        body = ""
+        conn_response = self.parse_url_and_connect(url)
+        if(conn_response == -1):
+            return conn_response
+
+        path = self.parsed_url.path
+        if not path:
+            path = "/"
+
+        #Join args
+        #Append args to path
+        
+        request = "GET {} HTTP/1.1\r\nHost: {}\r\nConnection: close\r\n\r\n".format(path, self.parsed_url.netloc)
+        self.sendall(request)
+        data = self.recvall(self.socket).strip()
+        self.close()
+        code = self.get_code(data)
+        headers = self.get_headers(data)
+        body = self.get_body(data)
         return HTTPResponse(code, body)
 
     def POST(self, url, args=None):
+        conn_response = self.parse_url_and_connect(url)
+        if(conn_response == -1):
+            return conn_response
+
+        request = "POST {} HTTP/1.1\r\nHost: {}\r\nContent-Type: {}\r\n"
+        
         code = 500
         body = ""
         return HTTPResponse(code, body)
 
     def command(self, url, command="GET", args=None):
+        conn_response = self.parse_url_and_connect(url)
+        if(conn_response == -1):
+            return conn_response
+
         if (command == "POST"):
             return self.POST( url, args )
         else:
